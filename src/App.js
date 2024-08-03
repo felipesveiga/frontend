f810f09757e210a91a9f81e436e28f10868cf0e9
felipesveiga <felipesveiga@gmail.com>
2024-08-03 10:52:14 -0300

    feature push

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React. Run test again
+          Learn React. Feature Env 
         </a>
       </header>
     </div>
